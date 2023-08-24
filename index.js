439d83ea0b97f7fe2021a5ce277c2567506bcd3c
DevWithEasy <devwitheasy@gmail.com>
2023-08-25 00:36:06 +0600

    csp

--- index.js
+++ index.js
@@ -7,14 +7,6 @@ const applyRouter = require('./routers/routes');
 const dbConnection = require('./config/dbConnection');
 const errorHandler = require('./middlewares/errorHandler');
 
-app.use(function (req, res, next) {
-    res.setHeader(
-        'Content-Security-Policy-Report-Only',
-        "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'"
-    );
-    next();
-});
-
 //serve client side file path
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.static(path.join(__dirname, './client/dist/')))
